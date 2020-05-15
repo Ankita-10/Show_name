86947dac73d3e5e21fac20c369a8a2e8c9a671fb
Ankita-10 <47694805+Ankita-10@users.noreply.github.com>
2020-05-15 19:43:03 +0530

    Add files via upload

---
+++ learnJava/src/main/java/com/example/learnjava/Microphone.java
@@ -0,0 +1,12 @@
+package com.example.learnjava;
+
+public class Microphone {
+//    String name;
+//    String color;
+//    int model;
+
+
+
+
+
+}
---
+++ learnJava/src/main/java/com/example/learnjava/MyClass.java
@@ -0,0 +1,7 @@
+package com.example.learnjava;
+
+public class MyClass {
+    public static void main(String[] args) {
+        System.out.println("ankita halder");
+    }
+}
